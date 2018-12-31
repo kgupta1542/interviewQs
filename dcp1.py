4d7460848accf26a33bfff173dceb2395f61b116
Kanishk Gupta <35873683+kgupta1542@users.noreply.github.com>
2018-12-31 14:08:04 -0800

    Create dcp1.py

---
+++ dcp1.py
@@ -0,0 +1,20 @@
+def findSumInList(x,k):
+	compls = [0] * (len(x))
+	isTrue = False;
+	
+	for i in range(len(x)):
+		compl = k - x[i];
+		
+		if x[i] in compls:
+			isTrue = True
+			break
+		else:
+			compls[i] = compl
+		
+	return isTrue
+			
+numList = [10,15,3,7]
+add = 17
+
+sumIsInList = findSumInList(numList,add)
+print(sumIsInList)
