4ea89eb1d6e0bf5595cf4c28c124d0fa85629475
Kanishk Gupta <35873683+kgupta1542@users.noreply.github.com>
2018-12-31 14:30:53 -0800

    Update dcp2.py

--- dcp2.py
+++ dcp2.py
@@ -1,13 +1,15 @@
+#Refer to Daily COding Problem #2
+
 def multiplyNumsInList(x):
-	product = 1
+	product = 1#Setup with default values
 	outputList = [1] * len(x)
 	output = []
 	
 	for i in range(len(x)):
-		product *= x[i]
-		outputList[i] = 1.0/x[i]
+		product *= x[i]#Multiplies each element in list
+		outputList[i] = 1.0/x[i]#Inserts reciprocal of element with corresponding index
 		
-	output = [int(i * product) for i in outputList]
+	output = [int(i * product) for i in outputList]#Multiplies through reciprocal list with product as constant
 	return output
 
 numList = [1,2,3,4,5]
