13007693daf457b3aedbba70ba8bca4d631d14ee
Kanishk Gupta <35873683+kgupta1542@users.noreply.github.com>
2018-12-31 14:28:08 -0800

    Update dcp1.py

--- dcp1.py
+++ dcp1.py
@@ -1,13 +1,15 @@
+#Refer to Daily Coding Problem #1
+
 def findSumInList(x,k):
-	compls = [0] * (len(x))
-	isTrue = False
+	compls = [0] * (len(x))#Creates empty list of zeroes
+	isTrue = False#Sets default value of boolean
 	
 	for i in range(len(x)):
-		compl = k - x[i]
+		compl = k - x[i]#Finds the 'complement' of each element
 		
-		if x[i] in compls:
+		if x[i] in compls:#Checks if the element is the 'complement' of another number
 			isTrue = True
-			break
+			break#Ends loop as objective is completed
 		
 		compls[i] = compl
 		
