d251c4eef26b0b98c61c10cd3f8ba2eaee1d9164
Kanishk Gupta <35873683+kgupta1542@users.noreply.github.com>
2019-01-06 18:23:34 -0800

    Update dcp4.py

--- Daily-Coding-Problem/dcp4.py
+++ Daily-Coding-Problem/dcp4.py
@@ -1,8 +1,3 @@
-#no upper,  no lower     -->   return lower
-#no upper,  yes lower    -->   return upper
-#yes upper, no lower     -->   return lower
-#yes upper,  yes lower   -->   return None
-
 def findMissingInt(x):#find lower and upper, checks if in x, declares as missing, finds smallest missing
 	missing = 0
 	temp = -1
@@ -14,18 +9,18 @@ def findMissingInt(x):#find lower and upper, checks if in x, declares as missing
 			
 			if upper in x or lower in x:#Fulfills last three reqs
 				temp = upper*(upper not in x) + lower*(lower not in x)
-				
-				if temp == 0:
-					temp = None
 		
 			else:
 				temp = lower*(lower != 0) + upper*(lower == 0)
 			
-			if (temp < missing and temp != None) or (missing == 0 and temp != None):
+			if (temp < missing or missing == 0) and temp != 0:
 				missing = temp
+	
+	if temp == -1:
+	    missing = 1
 			
 	return missing
 	
-numList = [4,3,1,2]
+numList = [1,3,6,4,2]#Missing Integer should be 2
 missingInt = findMissingInt(numList)
 print(missingInt)
