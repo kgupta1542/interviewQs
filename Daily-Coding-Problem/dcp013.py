fad33d5d949f9a10e4e5a0746889a4e067f9b5ba
Kanishk Gupta <35873683+kgupta1542@users.noreply.github.com>
2019-06-03 16:03:19 -0700

    Add files via upload

---
+++ Daily-Coding-Problem/dcp013.py
@@ -0,0 +1,24 @@
+def substrUniqueChars(k, s):#k - number of unique characters, s - input string
+    index = 0 #Starting index of substring
+    maxLength = 0 #Max length of substring
+    final = "" #Final substring
+    
+    for i in range(len(s)): #Run while the starting index is in bounds
+        index = i #Move up the starting index
+        uniqueCharCount = 0 #Set the amount of unique characters to 0
+        temp = "" #Set the temporary string to a blank string
+    
+        while uniqueCharCount <= k and index < len(s): #Run while the amount of unique characters is correct and the ending index is in bounds
+            if s[index] not in temp: #Check to see if character is unique
+                uniqueCharCount += 1
+            
+            temp += s[index] 
+            index += 1
+        
+        if maxLength < len(temp): #See if specific substring is longer than previous substrings
+            maxLength = len(temp) - (uniqueCharCount - k)
+            final = temp[0:len(temp) - (uniqueCharCount - k)]
+            
+    return maxLength, final
+
+print(substrUniqueChars(2,"abcba"))
